9efc7f3f1488214ad4dfff7d000980659b55f999
MAIN PC <main@pc>
2026-08-15 17:28:12 +0200

    setup project

---
+++ src/test/java/web/app/personalfinance/PersonalFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package web.app.personalfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
