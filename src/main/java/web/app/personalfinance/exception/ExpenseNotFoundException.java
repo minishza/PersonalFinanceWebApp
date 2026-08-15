b94e82c8822a7650d1c607356081e5902e878c3f
MAIN PC <main@pc>
2026-08-15 23:07:36 +0200

    add custom exceptions

---
+++ src/main/java/web/app/personalfinance/exception/ExpenseNotFoundException.java
@@ -0,0 +1,7 @@
+package web.app.personalfinance.exception;
+
+public class ExpenseNotFoundException extends RuntimeException {
+    public ExpenseNotFoundException(String message) {
+        super(message);
+    }
+}
