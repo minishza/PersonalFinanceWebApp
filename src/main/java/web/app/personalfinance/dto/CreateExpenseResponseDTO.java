760054e322111cdb132b5eff383c744f06857ab3
MAIN PC <main@pc>
2026-08-15 18:30:15 +0200

    add validation to expense creation dto

--- src/main/java/web/app/personalfinance/dto/CreateExpenseResponseDTO.java
+++ src/main/java/web/app/personalfinance/dto/CreateExpenseResponseDTO.java
@@ -1,5 +1,7 @@
 package web.app.personalfinance.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import web.app.personalfinance.enums.ExpenseCategory;
@@ -9,14 +11,17 @@ import java.time.LocalDate;
 
 @Builder
 @AllArgsConstructor
-public class CreateExpenseDTO {
+public class CreateExpenseResponseDTO {
+
+    @NotNull
+    @Positive(message = "pengarna måste vara mer än 0")
     private BigDecimal amount;
 
     private String description;
 
-
+    @NotNull
     private ExpenseCategory category;
 
-
+    @NotNull(message = "datum för handling måste finnas")
     private LocalDate expenseDate;
 }
